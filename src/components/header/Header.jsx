d707048cacb141761a70c0a75931e918456fb900
Nausheen Akhter <nausheen701@gmail.com>
2022-07-12 00:51:11 -0400

    img position

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-// import ME from '../../assets/me.jpg'
-import GREEN from '../../assets/green.png'
-import CREAM from '../../assets/cream.png'
+import ME from '../../assets/me.png'
+// import GREEN from '../../assets/green.png'
+// import CREAM from '../../assets/cream.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
@@ -13,14 +13,14 @@ const Header = () => {
         <h5>Hello I'm</h5>
         <h1>Nausheen Akhter</h1>
         <h5 className="text-light">Fullstack Developer</h5>
-        <CTA />
+        <CTA /> 
         <HeaderSocials/>
 
         <div className="me">
-            <img src={CREAM} height='1200' width='auto' alt="cream" />
+            <center><img src={ME} height='1200' width='auto' alt="me" /></center>
         </div>
-
-        <a href="contact" className='scroll__down'>Scroll Down</a>
+        <center><a href="contact" className='scroll__down'>Scroll Down</a> </center>
+        
       </div>
     </header> 
   )
--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -9,9 +9,7 @@ const HeaderSocials = () => {
     <div className='header__socials'>
         <a href="https://www.linkedin.com/in/nausheen-akhter-60b16b8a/" target="_blank"><BsLinkedin/></a> 
         <a href="https://github.com/Nausheen701" target="_blank"><FaGithub/></a> 
-        {/* <a href="https://www.facebook.com/nausheen.akhter.165/" target="_blank"></a>  */}
         </div>
-
   )
 }
 
