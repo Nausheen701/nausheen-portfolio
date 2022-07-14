ea47f56081620ce39de049ff0001c4937b756af1
Nausheen Akhter <nausheen701@gmail.com>
2022-07-14 02:30:45 -0400

    about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,51 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id='about'>About</section> 
+    <section id='about'>
+      <h5>Get to know</h5>
+      <h2>About me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image"/>
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            
+            <article className='about__card'>
+            <FaAward className='about__icon'/>
+            <h5>Experience</h5>
+            <small> 2 years </small>
+            </article>
+
+            <article className='about__card'>
+            <FiUsers className='about__icon'/>
+            <h5>Clients</h5>
+            <small> Many clients</small>
+            </article>
+
+            <article className='about__card'>
+            <VscFolderLibrary className='about__icon'/>
+            <h5>Projects</h5>
+            <small> 5 completed </small>
+            </article>
+            
+            <p> A paragraph</p>
+            <a href="#contact" className='btn'>Let's talk</a>
+          </div>
+        </div>
+
+      </div>
+    </section> 
   )
 }
 
