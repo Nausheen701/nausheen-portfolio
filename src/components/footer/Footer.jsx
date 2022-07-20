4d1f8a0554fa64aab7dfe9b16864e1ef49941a15
Nausheen Akhter <nausheen701@gmail.com>
2022-07-20 01:05:18 -0400

    footer socials

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
   return (
@@ -12,8 +14,18 @@ const Footer = () => {
           <li><a href="#portfolio">Portfolio</a></li>
           <li><a href="#skills">Technical skills</a></li>
           <li><a href="#contact">Contact me</a></li>
-
       </u1>
+
+      <div className="footer___socials"> 
+      <a href="https://www.linkedin.com/in/nausheen-akhter-60b16b8a/"><BsLinkedin/></a>
+      <a href="https://github.com/Nausheen701"><FaGithub/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>
+          {/* &copy; Nausheen Akhter Web developer. All rights reserved. */}
+        </small>
+      </div>
     </footer>
   )
 }
